6f40ec643400accca77867cb99f819c66d2faca5
Keith Uden <kuden@wwf.org.uk>
2013-11-25 17:59:06 +0000

    Adding function for random quote to site.js Thus far unsused

--- html/js/site.js
+++ html/js/site.js
@@ -35,3 +35,29 @@ $(".door-content .contentMag").click(
 
 
 });
+
+function getQuote(max) {
+    'use strict';
+    var arrQuotes = ["cool your jets", "easy tiger", "whoa nelly", "chill out man", "not yet", "step away", "calm down dear", "slow down dude", "nope, not yet", "wait your turn", "maybe tomorrow", "not today my friend", "patience, patience", "take it easy", "wait for it", "not so fast", "play it cool trig"];
+    
+    arrQuotes.sort(function (a, b) {
+        return (a.length - b.length);
+    });// sort array by item length
+
+    var maxPos = arrQuotes.length - 1;//default highest position in the array
+
+    if (max > 0) {// max length specified and a positive number
+        if (arrQuotes[0].length <= max) {
+            for (var j = 0; j < arrQuotes.length; j++) {
+                if (arrQuotes[j].length > max) {
+                    maxPos = j - 1;//find the position before the length is exceded
+                    break;
+                }
+            }
+        } else return "";// max specified but no results shorter than max
+    }
+
+    var i = Math.floor((Math.random() * (maxPos)) + 0.5);//pick a winner
+
+    return arrQuotes[i];
+}
